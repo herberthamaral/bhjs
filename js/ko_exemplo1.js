1d4de56db9cc215c361beb342b0279ed4c1431df
Herberth Amaral <herberthamaral@gmail.com>
2012-01-21 05:51:58 -0200

    1.0 rc

--- js/ko_exemplo1.js
+++ js/ko_exemplo1.js
@@ -4,7 +4,7 @@ var viewModel = {
     onTwitter: ko.observable(false),
     twitter: ko.observable(''),
     submit: function(){
-        alert(JSON.stringify(pko.toJS(viewModel)));
+        alert(JSON.stringify(ko.toJS(viewModel)));
     }.bind(this)
 };             
 ko.applyBindings(viewModel, document.getElementById('ko-exemplo1'));
